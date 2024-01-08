2069cab4afa712e4f0149b8cce3c6cddf81dcaa5
a_cizek <a_cizek@utb.cz>
2024-01-08 15:19:13 +0100

    Created methods to get countries by name and region from API

--- app/src/main/java/com/example/countries/api/CountriesApiService.kt
+++ app/src/main/java/com/example/countries/api/CountriesApiService.kt
@@ -1,4 +1,23 @@
 package com.example.countries.api
 
-class CountriesApiService {
+import com.example.countries.model.Country
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CountriesApiService {
+    companion object {
+        const val SEARCH_BY_NAME_ENDPOINT = "https://restcountries.com/v3.1/name"
+        const val SEARCH_BY_REGION_ENDPOINT = "https://restcountries.com/v3.1/region"
+    }
+
+    @GET("$SEARCH_BY_NAME_ENDPOINT/{countryName}")
+    suspend fun getCountriesByName(
+        @Path("countryName") countryName: String
+    ): Response<List<Country>>
+
+    @GET("$SEARCH_BY_REGION_ENDPOINT/{countryName}")
+    suspend fun getCountriesByRegion(
+        @Path("countryName") countryName: String
+    ): Response<List<Country>>
 }
