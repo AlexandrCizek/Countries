ed9dcc56d9aa6c45661f32c670f74b4f482797df
a_cizek <a_cizek@utb.cz>
2024-01-08 15:31:25 +0100

    Added methods to CountryRepository.kt to get countries by name and region using CountriesApiService

--- app/src/main/java/com/example/countries/CountryRepository.kt
+++ app/src/main/java/com/example/countries/CountryRepository.kt
@@ -1,7 +1,26 @@
 package com.example.countries
 
 import com.example.countries.api.CountriesApiService
+import com.example.countries.model.Country
 
-class CountryRepository (private val countriesApiService: CountriesApiService) {
+class CountryRepository(private val countriesApiService: CountriesApiService) {
+    suspend fun getCountriesByName(name: String) : List<Country>? {
+        val response = countriesApiService.getCountriesByName(name)
 
+        if(response.isSuccessful) {
+            return response.body()
+        } else {
+            return null
+        }
+    }
+
+    suspend fun getCountriesByRegion(region: String) : List<Country>? {
+        val response = countriesApiService.getCountriesByRegion(region)
+
+        if(response.isSuccessful) {
+            return response.body()
+        } else {
+            return null
+        }
+    }
 }
