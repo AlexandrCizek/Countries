7e026ed602a220b271222e3ea1356cf34a195c2d
a_cizek <a_cizek@utb.cz>
2024-01-09 13:07:49 +0100

    Injected countries repository to CountriesListViewModel constructor. Created variables to hold List of countries. Added method to get countries by name and populate the list.

--- app/src/main/java/com/example/countries/CountriesListViewModel.kt
+++ app/src/main/java/com/example/countries/CountriesListViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.countries
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.countries.model.Country
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class CountriesListViewModel : ViewModel() {
+class CountriesListViewModel(private val repository: CountryRepository) : ViewModel() {
+    private val _countries = MutableLiveData<List<Country>>()
+    val countries : LiveData<List<Country>> = _countries
 
+    fun getCountriesByName(name: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = repository.getCountriesByName(name)
+            _countries.postValue(result)
+        }
+    }
 }
