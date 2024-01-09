2c40263768e0ac6b3617624e2521af575c83f030
a_cizek <a_cizek@utb.cz>
2024-01-09 13:04:37 +0100

    Added CountriesListViewModel to CountriesListActivity. Created new app context with MyApplication class.

--- app/src/main/java/com/example/countries/CountriesListActivity.kt
+++ app/src/main/java/com/example/countries/CountriesListActivity.kt
@@ -2,18 +2,35 @@ package com.example.countries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.countries.databinding.ActivityCountriesListBinding
 
 class CountriesListActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityCountriesListBinding
+    private lateinit var countriesListBinding: ActivityCountriesListBinding
+    private lateinit var countriesListViewModel: CountriesListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_countries_list)
 
-        binding = ActivityCountriesListBinding.inflate(layoutInflater)
-        val view = binding.root
+        val appContext = application
+        val app = appContext as? MyApplication ?: run {
+            Log.e("CountriesListActivity", "Application context is not MyApplication")
+            return
+        }
+
+        countriesListBinding = ActivityCountriesListBinding.inflate(layoutInflater)
+        val view = countriesListBinding.root
         setContentView(view)
+
+        countriesListViewModel = ViewModelProvider(this, CountriesListViewModelFactory(app.countriesRepository))
+            .get(CountriesListViewModel::class.java)
+
+        countriesListBinding.countriesListViewModel = countriesListViewModel
+        countriesListBinding.lifecycleOwner = this
+
+        countriesListViewModel.getCountriesByName("Germany")
     }
 }
