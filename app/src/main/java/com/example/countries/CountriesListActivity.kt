bfb499a0493306ab14fccf8324acb77251b2dffd
a_cizek <a_cizek@utb.cz>
2024-01-09 16:16:16 +0100

    Added observer in CountriesListActivity to observe changes of countries list to create new ListView adapter when the list changes. Added click gesture to navigate to CountriesListDetailActivity with new intent.

--- app/src/main/java/com/example/countries/CountriesListActivity.kt
+++ app/src/main/java/com/example/countries/CountriesListActivity.kt
@@ -1,19 +1,19 @@
 package com.example.countries
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.countries.databinding.ActivityCountriesListBinding
 
 class CountriesListActivity : AppCompatActivity() {
 
-    private lateinit var countriesListBinding: ActivityCountriesListBinding
-    private lateinit var countriesListViewModel: CountriesListViewModel
-
+    private lateinit var binding: ActivityCountriesListBinding
+    private lateinit var viewModel: CountriesListViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_countries_list)
 
         val appContext = application
         val app = appContext as? MyApplication ?: run {
@@ -21,14 +21,31 @@ class CountriesListActivity : AppCompatActivity() {
             return
         }
 
-        countriesListBinding = ActivityCountriesListBinding.inflate(layoutInflater)
-        val view = countriesListBinding.root
+        binding = ActivityCountriesListBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(view)
 
-        countriesListViewModel = ViewModelProvider(this, CountriesListViewModelFactory(app.countriesRepository))
+        viewModel = ViewModelProvider(this,
+            CountriesListViewModelFactory(app.countriesRepository))
             .get(CountriesListViewModel::class.java)
 
-        countriesListBinding.countriesListViewModel = countriesListViewModel
-        countriesListBinding.lifecycleOwner = this
+        binding.countriesListViewModel = viewModel
+        binding.lifecycleOwner = this
+
+        viewModel.countries.observe(this, Observer { countries ->
+            val adapter = CountriesListAdapter(this, countries)
+            binding.countriesList.adapter = adapter
+        })
+
+        binding.countriesList.isClickable = true
+        binding.countriesList.setOnItemClickListener { parent, view, position, id ->
+            val country = viewModel.countries.value?.get(position)
+            country?.let {
+                val intent = Intent(this, CountriesListDetailActivity::class.java)
+                intent.putExtra("COUNTRY_NAME", it.name.common)
+                intent.putExtra("COUNTRY_POPULATION", it.population)
+                startActivity(intent)
+            }
+        }
     }
 }
