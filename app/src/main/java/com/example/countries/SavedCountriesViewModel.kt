d561be285e4371c2570cfaba825268cd6de9746f
a_cizek <a_cizek@utb.cz>
2024-01-11 11:31:45 +0100

    Created SavedCountriesViewModel. Added method to get all saved countries from Room database and update the _countries property. This method is called when ViewModel is initalized.

---
+++ app/src/main/java/com/example/countries/SavedCountriesViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.countries
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.countries.model.Country
+import kotlinx.coroutines.launch
+
+class SavedCountriesViewModel(private val repository: CountryRepository) : ViewModel() {
+    private val _countries = MutableLiveData<List<Country>>()
+    val countries : LiveData<List<Country>> = _countries
+
+    init {
+        getSavedCountries()
+    }
+
+    private fun updateCountriesList(countries: List<Country>?) {
+        countries?.let {
+            val sortedCountries = countries.sortedBy { it.name.common }
+            _countries.postValue(sortedCountries)
+            return
+        }
+    }
+
+    fun getSavedCountries() {
+        viewModelScope.launch {
+            val result = repository.getSavedCountries()
+            updateCountriesList(result)
+        }
+    }
+}
