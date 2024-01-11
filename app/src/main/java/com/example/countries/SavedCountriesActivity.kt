e2032e5379aab2a42447adf97b72e7270f19ba9d
a_cizek <a_cizek@utb.cz>
2024-01-11 11:29:50 +0100

    Set ViewModel from the SavedCountriesViewModelFactory. Added observer for ListView to update rows using CountriesListAdapter. Passed information that the action type is "DELETE" when going from SavedCountriesActivity.

--- app/src/main/java/com/example/countries/SavedCountriesActivity.kt
+++ app/src/main/java/com/example/countries/SavedCountriesActivity.kt
@@ -1,14 +1,16 @@
 package com.example.countries
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.example.countries.databinding.ActivityCountriesListBinding
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
 import com.example.countries.databinding.ActivitySavedCountriesBinding
 
 class SavedCountriesActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySavedCountriesBinding
-
+    private lateinit var viewModel: SavedCountriesViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,5 +23,30 @@ class SavedCountriesActivity : AppCompatActivity() {
         binding = ActivitySavedCountriesBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        viewModel = ViewModelProvider(this,
+            SavedCountriesViewModelFactory(app.countriesRepository))
+            .get(SavedCountriesViewModel::class.java)
+
+        viewModel.countries.observe(this, Observer { countries ->
+            val adapter = CountriesListAdapter(this, countries)
+            binding.savedCountriesList.adapter = adapter
+        })
+
+        binding.savedCountriesList.isClickable = true
+        binding.savedCountriesList.setOnItemClickListener { parent, view, position, id ->
+            val country = viewModel.countries.value?.get(position)
+            country?.let {
+                val intent = Intent(this, CountriesListDetailActivity::class.java)
+                intent.putExtra("COUNTRY_NAME", it.name.common)
+                intent.putExtra("COUNTRY_FLAGS", it.flags.png)
+                intent.putExtra("COUNTRY_POPULATION", it.population)
+                intent.putExtra("COUNTRY_CONTINENT", it.continents.first())
+                intent.putExtra("COUNTRY_CAPITAL", it.capital.first())
+                intent.putExtra("COUNTRY_FLAG", it.flag)
+                intent.putExtra("ACTION_TYPE", "DELETE")
+                startActivity(intent)
+            }
+        }
     }
 }
